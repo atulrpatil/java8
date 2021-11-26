e2c63154a22dfd18b24498dd7884a1748e67a247
TejaswiniKhairnar <46070942+TejaswiniKhairnar@users.noreply.github.com>
2021-11-26 20:50:00 +0530

    Update ArraaySortDemo.java

--- ArraaySortDemo.java
+++ ArraaySortDemo.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class ArraaySortDemo {
 
 	// Write a Java 8 program to sort an array and then convert the sorted array
-	// into Stream
+	// into 
 	public static void main(String[] args) {
 
 		Integer[] array = { 99, 55, 203, 99, 4, 91 };
